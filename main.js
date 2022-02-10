3ed4083d03dfd242c65086ad44148c0d143198fa
joecrypt <oyakhilomejoseph12@gmail.com>
2022-02-10 23:40:48 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,43 @@
+
+ var button1 = document.getElementById("btn1");
+ var button2 = document.getElementById("btn2");
+ var button3 = document.getElementById("btn3");
+function theme1() {
+  document.body.classList.add("body");
+  document.body.classList.remove("theme2");
+  document.body.classList.remove("theme3");
+  document.getElementById("btn2").style.opacity = "0.1";
+  document.getElementById("btn3").style.opacity = "0.1";
+}
+
+function theme2() {
+  document.body.classList.add("theme2");
+  document.body.classList.remove("body");
+  document.body.classList.remove("theme3");
+  document.getElementById("btn1").style.opacity = "0.1";
+  document.getElementById("btn3").style.opacity = "0.1";
+}
+
+function theme3() {
+  document.body.classList.add("theme3");
+  document.body.classList.remove("theme2");
+  document.body.classList.remove("body");
+  document.getElementById("btn2").style.opacity = "0.1";
+  document.getElementById("btn1").style.opacity = "0.1";
+   document.getElementById("equalTo").style.color = "black";
+}
+
+button1.onclick =function() {
+  theme1();
+  
+}
+
+button2.onclick =function() {
+  theme2();
+  
+}
+
+button3.onclick =function() {
+  theme3();
+ 
+}
